827140550d4202787b010af00ca127918b5c199b
Rory Primrose <roryprimrose@microsoft.com>
2023-07-27 12:16:27 +1000

    Added documentation

--- Neovolve.Streamline.NSubstitute/TestsSubstituteOf.cs
+++ Neovolve.Streamline.NSubstitute/TestsSubstituteOf.cs
@@ -3,12 +3,22 @@
 using System;
 using System.Reflection;
 
+/// <summary>
+/// The <see cref="TestsSubstituteOf{T}"/> class defines the base class for NSubstitute-based tests.
+/// </summary>
+/// <typeparam name="T">The type of the class under test.</typeparam>
+/// <remarks>
+/// This class uses <see cref="Substitute.For"/> to create services required when reading from <see cref="SUT" />
+/// and uses <see cref="Substitute.For"/> to create <see cref="SUT" /> itself.
+/// This class is typically used when a full mock of the class under test is required.
+/// </remarks>
 public abstract class TestsSubstituteOf<T> : Tests<T> where T : class
 {
     protected TestsSubstituteOf(params object[] services) : base(services)
     {
     }
         
+    /// <inheritdoc />
     protected override T BuildSUT(ConstructorInfo constructor, object[] parameterValues)
     {
         Type[] types = { TargetType };
